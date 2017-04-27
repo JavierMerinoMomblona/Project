3280e4691e4e57ad115aa4e6d5af64574bdff5bb
JavierMerinoMomblona <j.m.m.1995@hotmail.com>
2017-04-27 18:03:45 +0200

    Configuracion Base Pública

--- app/admin.py
+++ app/admin.py
@@ -1,12 +1,2 @@
 from django.contrib import admin
-from .models import Categoria, Usuario, Link, Archivo, Auxiliar, Inventario, Mensajes, SustratosOriginales, SustratosCatalogados
-
-admin.site.register(Categoria)
-admin.site.register(Usuario)
-admin.site.register(Link)
-admin.site.register(Archivo)
-admin.site.register(Auxiliar)
-admin.site.register(Inventario)
-admin.site.register(Mensajes)
-admin.site.register(SustratosOriginales)
-admin.site.register(SustratosCatalogados)
+from .models import *
